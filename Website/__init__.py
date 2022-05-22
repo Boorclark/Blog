bf1fae3ffe8da091b3a614248f1af66d3e0d2f67
Boorclark <clarkg@berea.edu>
2022-05-22 01:25:37 -0400

    Adjusted auth code. Commiting before changes to HTML for flash messages

--- Website/__init__.py
+++ Website/__init__.py
@@ -23,7 +23,7 @@ def create_app():
 
     create_database(app)
 
-    login_manager = LoginManager
+    login_manager = LoginManager()
     login_manager.login_view = "auth.login" # if not logged in redirect user to this
     login_manager.init_app(app)
 
--- Website/auth.py
+++ Website/auth.py
@@ -1,16 +1,31 @@
+from unicodedata import category
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 
 
+
 @auth.route("/login", methods=['GET', 'POST']) # allows get and post w/ out error
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+    
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    
     return render_template("login.html")
 
 
@@ -22,8 +37,8 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
     
-        email_exists = User.query.filer_by(email=email).first() # looks to see if this is the only email in db
-        username_exists =  User.query.filer_by(username=username).first()
+        email_exists = User.query.filter_by(email=email).first() # looks to see if this is the only email in db
+        username_exists =  User.query.filter_by(username=username).first()
         
         if email_exists:
             flash('Email is already in use.', category='error') # flashes this message to user
@@ -38,15 +53,18 @@ def sign_up():
         elif len(email) < 4:                                    # ADD IN CODE TO CHECK FOR VAILD EMAILS LATER
             flash('Email is invalid.', category='error')
         else:
-            new_user = User(email=email, username=username, password=password1)
-            db.session.ass(new_user)
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)                                                    # sha256 outputs hash that is 256 bits long
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('User Created!')
             return redirect(url_for('views.home'))
 
     return render_template("signup.html")
-
+ 
 
 @auth.route("/logout")
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
