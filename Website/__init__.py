4b6d57cce17e4604b0ff0f0dba9e48c6a0ecba3b
Boorclark <clarkg@berea.edu>
2022-05-21 22:59:53 -0400

    Created basic page templates and file folders

--- Website/__init__.py
+++ Website/__init__.py
@@ -5,13 +5,13 @@ from flask_login import LoginManager
 
 
 def create_app():
-    app = Flask(__name__)  # name of module running to create app
+    app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
 
     from .views import views
     from .auth import auth
-    
-    app. register_blueprint(views, url_prefix="/")
-    app. register_blueprint(views, url_prefix="/")
-    
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
