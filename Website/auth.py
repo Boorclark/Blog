1b5bfb850e4ffb1878d5c62b39d9f3cdf7f33d54
Boorclark <clarkg@berea.edu>
2022-05-22 00:24:53 -0400

    Created code for signup validation

--- Website/auth.py
+++ Website/auth.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from curses import flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 
+from Website import views
+from . import DB_NAME, db
+from .models import User
 
 auth = Blueprint("auth", __name__)
 
@@ -13,11 +17,33 @@ def login():
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
-    email = request.form.get("email")
-    username = request.form.get("username")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
     
+        email_exists = User.query.filer_by(email=email).first() # looks to see if this is the only email in db
+        username_exists =  User.query.filer_by(username=username).first()
+        if email_exists:
+            flash('Email is already in use.', category='error') # flashes this message to user
+        elif username_exists:
+            flash('Username is already in use.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(username) < 4:
+            flash('Username is too short.', category='error')
+        elif len(password1) < 6:
+            flash('Password is too short.', category='error')
+        elif len(email) < 4:                                    # ADD IN CODE TO CHECK FOR VAILD EMAILS LATER
+            flash('Email is invalid.', category='error')
+        else:
+            new_user = User(email=email, username=username, password=password1)
+            db.session.ass(new_user)
+            db.session.commit()
+            flash('User Created!')
+            return redirect(url_for('views.home'))
+            
     return render_template("signup.html")
 
 
