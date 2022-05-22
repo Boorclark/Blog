cba36fc40ab181e9524d04d4caffabf96a4c0089
Boorclark <clarkg@berea.edu>
2022-05-22 12:55:47 -0400

    Like model and function

--- Website/models.py
+++ Website/models.py
@@ -28,3 +28,11 @@ class Comment(db.Model):
         'user.id', ondelete="CASCADE"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey(
         'post.id', ondelete="CASCADE"), nullable=False)
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'post.id', ondelete="CASCADE"), nullable=False)
